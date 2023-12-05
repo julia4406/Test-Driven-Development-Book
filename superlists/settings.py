5dde7dad8edc699f918d7752f18b6755181fac6d
Yulia_Alekseeva <julia4406@gmail.com>
2023-12-05 12:38:28 +0200

    A little change in DEBUG line of settings.py.

--- superlists/settings.py
+++ superlists/settings.py
@@ -29,7 +29,8 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'django-insecure-0uur_=4d!d!o@v%$7l%319j)!!uxfpy8r$#@8-jp)5%=c78m-f'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+#DEBUG = False
+DEBUG = bool( os.environ.get('DJANGO_DEBUG', True) )
 
 ALLOWED_HOSTS = [
     "http://superlists-staging.perscloud.pp.ua",
